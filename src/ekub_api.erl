dcbfa3e2b1211896e9f5d23ac2173632d9032f15
Anton Alferov <anton@alferov.me>
2018-06-27 11:54:01 +0200

    Add exec command and some refactoring

--- src/ekub_api.erl
+++ src/ekub_api.erl
@@ -2,29 +2,38 @@
 
 -export([
     pods/1, pods/2, pods/3,
+    exec/5,
     version/1
 ]).
 
 -define(Core, ekub_core).
 
--define(Api, "/api/v1").
+-define(ExecQuery, [
+    {"stdout", "true"},
+    {"stderr", "true"}
+]).
 
 pods(Access) ->
-    get(pods, Access).
+    get("/api/v1/pods", Access).
 
 pods(Namespace, Access) ->
-    get(path([namespaces, Namespace, pods]), Access).
+    get("/api/v1/namespace/~s/pods", [Namespace], Access).
 
 pods(Namespace, Pod, Access) ->
-    get(path([namespaces, Namespace, pods, Pod]), Access).
+    get("/api/v1/namespace/~s/pods/~s", [Namespace, Pod], Access).
 
-version(Access) ->
-    ?Core:http_request("/version", Access).
+exec(Namespace, PodName, ContainerName, Command, Access) ->
+    Resource = fmt("/api/v1/namespaces/~s/pods/~s/exec", [Namespace, PodName]),
 
-get(Path, Access) ->
-    ?Core:http_request(?Api ++ [$/|Path], Access).
+    Query = [{"container", ContainerName}|?ExecQuery] ++
+            [{"command", Arg} || Arg <- string:split(Command, " ", all)],
 
-path(Items) -> lists:flatten(lists:join($/, [to_list(Item) || Item <- Items])).
+    ?Core:ws_request(Resource, Query, Access).
 
-to_list(L) when is_list(L) -> L;
-to_list(A) when is_atom(A) -> atom_to_list(A).
+version(Access) -> get("/version", Access).
+
+get(Resource, Access) -> get(Resource, [], Access).
+get(Resource, Args, Access) -> ?Core:http_request(fmt(Resource, Args), Access).
+
+fmt(Format, Args) ->
+    lists:flatten(io_lib:format(Format, Args)).
