b4987ca904d1440e75019831fc90c28f9581748d
Anton Alferov <anton@alferov.me>
2018-07-09 23:26:35 +0200

    Add deployment examples

--- src/ekub_examples.erl
+++ src/ekub_examples.erl
@@ -4,20 +4,23 @@
     namespace_create/0,
     namespace_delete/0,
 
-    pod_yaml_create/0,
-    pod_map_create/0,
-
-    pod_yaml_patch/0,
-    pod_map_patch/0,
+    pod_create/0,
+    pod_patch/0,
 
     pod_list/0,
+    pod_exec/0,
 
-    pod_yaml_delete/0,
-    pod_map_delete/0
+    pod_delete/0,
+
+    deployment_create/0,
+    deployment_delete/0
 ]).
 
 -define(ApiNs, ekub_api_namespace).
 -define(ApiPod, ekub_api_pod).
+-define(ApiDeploy, ekub_api_deployment).
+
+% All manifests could be defined as either a Yaml document or an Erlang map
 
 namespace_create() -> ?ApiNs:create(
     "
@@ -34,63 +37,54 @@ namespace_delete() -> ?ApiNs:delete(
     access()
 ).
 
-pod_yaml_create() -> ?ApiPod:create(
+pod_create() -> ?ApiPod:create(
     "ekub-example", % namespace
     "
      apiVersion: v1
      kind: Pod
      metadata:
-       name: ekub-example-yaml
+       name: ekub-example
      spec:
        containers:
-       - name: ekub-example-yaml
+       - name: ekub-example
          image: aialferov/pause:1.0.0
     ",
+    %%% map based example
+    %
+    % #{apiVersion => <<"v1">>,
+    %   kind => <<"Pod">>,
+    %   metadata =>
+    %     #{name => <<"ekub-example">>},
+    %   spec =>
+    %     #{containers => [
+    %       #{name => <<"ekub-example">>,
+    %         image => <<"aialferov/pause:1.0.0">>}
+    %     ]}}
+    %%%
     access()
 ).
 
-pod_map_create() -> ?ApiPod:create(
+pod_patch() -> ?ApiPod:patch(
     "ekub-example", % namespace
-
-    #{apiVersion => <<"v1">>,
-      kind => <<"Pod">>,
-      metadata =>
-        #{name => <<"ekub-example-map">>},
-      spec =>
-        #{containers => [
-          #{name => <<"ekub-example-map">>,
-            image => <<"aialferov/pause:1.0.0">>}
-        ]}},
-
-    access()
-).
-
-pod_yaml_patch() -> ?ApiPod:patch(
-    "ekub-example", % namespace
-    "ekub-example-yaml", % pod name
+    "ekub-example", % pod name
     "
      apiVersion: v1
      kind: Patch
      spec:
        containers:
-       - name: ekub-example-yaml
+       - name: ekub-example
          image: aialferov/pause:1.1.0
     ",
-    access()
-).
-
-pod_map_patch() -> ?ApiPod:patch(
-    "ekub-example", % namespace
-    "ekub-example-map", % pod name
-
-    #{apiVersion => <<"v1">>,
-      kind => <<"Patch">>,
-      spec =>
-        #{containers => [
-          #{name => <<"ekub-example-map">>,
-            image => <<"aialferov/pause:1.1.0">>}
-        ]}},
-
+    %%% map based example
+    %
+    % #{apiVersion => <<"v1">>,
+    %   kind => <<"Patch">>,
+    %   spec =>
+    %     #{containers => [
+    %       #{name => <<"ekub-example">>,
+    %         image => <<"aialferov/pause:1.1.0">>}
+    %     ]}},
+    %%%
     access()
 ).
 
@@ -99,16 +93,51 @@ pod_list() -> ?ApiPod:list(
     access()
 ).
 
-pod_yaml_delete() -> ?ApiPod:delete(
+pod_exec() -> ?ApiPod:exec(
     "ekub-example", % namespace
-    "ekub-example-yaml", % pod name
+    "ekub-example", % pod name
+    "ekub-example", % container name
+    "pause version", % command
+    % 10000, % optional timeout in ms
     access()
 ).
 
-pod_map_delete() -> ?ApiPod:delete(
-    "ekub-example", % namespace
-    "ekub-example-map", % pod name
+pod_delete() -> ?ApiPod:delete(
+    "ekub-example",
+    "ekub-example",
     access()
 ).
 
-access() -> {ok, Access} = ekub_access:read(), Access.
+deployment_create() -> ?ApiDeploy:create(
+    "ekub-example", % namespace
+    "
+     apiVersion: apps/v1beta1
+     kind: Deployment
+     metadata:
+       name: ekub-example
+       labels:
+         app: ekub-example
+     spec:
+       replicas: 3
+       template:
+         metadata:
+           labels:
+             app: ekub-example
+         spec:
+           containers:
+           - name: ekub-example
+             image: aialferov/pause:1.0.0
+    ",
+    access()
+).
+
+deployment_delete() -> ?ApiDeploy:delete(
+    "ekub-example", % namespace
+    "ekub-example", % deployment name
+    [{orphan_dependents, false}],
+    access()
+).
+
+access() ->
+    {ok, Access} = ekub_access:read(),
+    Access.
