71380f77f2e6c38c6eb9d1c898f5ec1c6ca846bf
Anton Alferov <anton@alferov.me>
2018-07-09 14:19:14 +0200

    Pod API refactoring

--- src/ekub_api_pod.erl
+++ src/ekub_api_pod.erl
@@ -20,62 +20,66 @@
 
 -define(Core, ekub_core).
 
+-define(ApiPod, "/api/v1/namespaces/~s/pods/~s").
+-define(ApiPods, "/api/v1/namespaces/~s/pods").
+-define(ApiPodsAllNs, "/api/v1/pods").
+
+-define(ApiWatchPod, "/api/v1/watch/namespaces/~s/pods/~s").
+-define(ApiWatchPods, "/api/v1/watch/namespaces/~s/pods").
+-define(ApiWatchPodsAllNs, "/api/v1/watch/pods").
+
+-define(ApiPodExec, "/api/v1/namespaces/~s/pods/~s/exec").
+-define(ApiPodLog, "/api/v1/namespaces/~s/pods/~s/log").
+
 create(Namespace, Pod, Access) ->
-    Resource = {"/api/v1/namespaces/~s/pods", [Namespace]},
-    ?Core:http_request(post, Resource, [], Pod, Access).
+    ?Core:http_request(post, {?ApiPods, [Namespace]}, [], Pod, Access).
 
 patch(Namespace, PodName, Patch, Access) ->
-    Resource = {"/api/v1/namespaces/~s/pods/~s", [Namespace, PodName]},
+    Resource = {?ApiPod, [Namespace, PodName]},
     ?Core:http_request(patch, Resource, [], Patch, Access).
 
 delete(Namespace, PodName, Access) -> delete(Namespace, PodName, [], Access).
 delete(Namespace, PodName, Options, Access) ->
-    Resource = {"/api/v1/namespaces/~s/pods/~s", [Namespace, PodName]},
+    Resource = {?ApiPod, [Namespace, PodName]},
     ?Core:http_request(delete, Resource, Options, Access).
 
 read(Namespace, PodName, Access) -> read(Namespace, PodName, [], Access).
 read(Namespace, PodName, Options, Access) ->
-    Resource = {"/api/v1/namespaces/~s/pods/~s", [Namespace, PodName]},
-    ?Core:http_request(Resource, Options, Access).
+    ?Core:http_request({?ApiPod, [Namespace, PodName]}, Options, Access).
 
 list(Namespace, Access) -> list(Namespace, [], Access).
 list(Namespace, Options, Access) ->
-    Resource = {"/api/v1/namespaces/~s/pods", [Namespace]},
-    ?Core:http_request(Resource, Options, Access).
+    ?Core:http_request({?ApiPods, [Namespace]}, Options, Access).
 
 list_all_namespaces(Access) -> list_all_namespaces([], Access).
 list_all_namespaces(Options, Access) ->
-    ?Core:http_request("/api/v1/pods", Options, Access).
+    ?Core:http_request(?ApiPodsAllNs, Options, Access).
 
 watch(Namespace, PodName, Access) -> watch(Namespace, PodName, [], Access).
 watch(Namespace, PodName, Options, Access) ->
-    Resource = {"/api/v1/watch/namespaces/~s/pods/~s", [Namespace, PodName]},
+    Resource = {?ApiWatchPod, [Namespace, PodName]},
     ?Core:http_stream_request(Resource, Options, Access).
 
 watch_list(Namespace, Access) -> watch_list(Namespace, [], Access).
 watch_list(Namespace, Options, Access) ->
-    Resource = {"/api/v1/watch/namespaces/~s/pods", [Namespace]},
-    ?Core:http_stream_request(Resource, Options, Access).
+    ?Core:http_stream_request({?ApiWatchPods, [Namespace]}, Options, Access).
 
 watch_list_all_namespaces(Access) -> watch_list_all_namespaces([], Access).
 watch_list_all_namespaces(Options, Access) ->
-    ?Core:http_stream_request("/api/v1/watch/pods", Options, Access).
+    ?Core:http_stream_request(?ApiWatchPodsAllNs, Options, Access).
 
 watch(Ref) -> ?Core:http_stream_read(Ref).
 
 exec(Namespace, PodName, ContainerName, Command, Access) ->
-    Resource = {"/api/v1/namespaces/~s/pods/~s/exec", [Namespace, PodName]},
-
     Query = [{"stdout", "true"},
              {"stderr", "true"},
              {"container", ContainerName}|
              [{"command", Arg} || Arg <- string:split(Command, " ", all)]],
 
-    ?Core:ws_request(Resource, Query, Access).
+    ?Core:ws_request({?ApiPodExec, [Namespace, PodName]}, Query, Access).
 
 read_log(Namespace, PodName, Access) ->
     read_log(Namespace, PodName, [], Access).
 
 read_log(Namespace, PodName, Options, Access) ->
-    Resource = {"/api/v1/namespaces/~s/pods/~s/log", [Namespace, PodName]},
-    ?Core:http_request(Resource, Options, Access).
+    ?Core:http_request({?ApiPodLog, [Namespace, PodName]}, Options, Access).
