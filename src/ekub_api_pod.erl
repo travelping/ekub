1bbb0e25e9e4efbfe8b693d995bc4cfa8d484433
Anton Alferov <anton@alferov.me>
2018-07-10 08:16:51 +0200

    Add ability to specify timeout in pod exec
    Also refactoring

--- src/ekub_api_pod.erl
+++ src/ekub_api_pod.erl
@@ -13,7 +13,7 @@
     watch_list/2, watch_list/3,
     watch_list_all_namespaces/1, watch_list_all_namespaces/2,
 
-    exec/5,
+    exec/5, exec/6,
 
     read_log/3, read_log/4
 ]).
@@ -32,54 +32,68 @@
 -define(ApiPodLog, "/api/v1/namespaces/~s/pods/~s/log").
 
 create(Namespace, Pod, Access) ->
-    ?Core:http_request(post, {?ApiPods, [Namespace]}, [], Pod, Access).
+    Resource = {?ApiPods, [Namespace]},
+    ?Core:http_request(post, Resource, [], Pod, Access).
 
 patch(Namespace, PodName, Patch, Access) ->
     Resource = {?ApiPod, [Namespace, PodName]},
     ?Core:http_request(patch, Resource, [], Patch, Access).
 
 delete(Namespace, PodName, Access) -> delete(Namespace, PodName, [], Access).
-delete(Namespace, PodName, Options, Access) ->
+delete(Namespace, PodName, Query, Access) ->
     Resource = {?ApiPod, [Namespace, PodName]},
-    ?Core:http_request(delete, Resource, Options, Access).
+    ?Core:http_request(delete, Resource, Query, Access).
 
 read(Namespace, PodName, Access) -> read(Namespace, PodName, [], Access).
-read(Namespace, PodName, Options, Access) ->
-    ?Core:http_request({?ApiPod, [Namespace, PodName]}, Options, Access).
+read(Namespace, PodName, Query, Access) ->
+    Resource = {?ApiPod, [Namespace, PodName]},
+    ?Core:http_request(Resource, Query, Access).
 
 list(Namespace, Access) -> list(Namespace, [], Access).
-list(Namespace, Options, Access) ->
-    ?Core:http_request({?ApiPods, [Namespace]}, Options, Access).
+list(Namespace, Query, Access) ->
+    Resource = {?ApiPods, [Namespace]},
+    ?Core:http_request(Resource, Query, Access).
 
 list_all_namespaces(Access) -> list_all_namespaces([], Access).
-list_all_namespaces(Options, Access) ->
-    ?Core:http_request(?ApiPodsAllNs, Options, Access).
+list_all_namespaces(Query, Access) ->
+    Resource = ?ApiPodsAllNs,
+    ?Core:http_request(Resource, Query, Access).
 
 watch(Namespace, PodName, Access) -> watch(Namespace, PodName, [], Access).
-watch(Namespace, PodName, Options, Access) ->
+watch(Namespace, PodName, Query, Access) ->
     Resource = {?ApiWatchPod, [Namespace, PodName]},
-    ?Core:http_stream_request(Resource, Options, Access).
+    ?Core:http_stream_request(Resource, Query, Access).
 
 watch_list(Namespace, Access) -> watch_list(Namespace, [], Access).
-watch_list(Namespace, Options, Access) ->
-    ?Core:http_stream_request({?ApiWatchPods, [Namespace]}, Options, Access).
+watch_list(Namespace, Query, Access) ->
+    Resource = {?ApiWatchPods, [Namespace]},
+    ?Core:http_stream_request(Resource, Query, Access).
 
 watch_list_all_namespaces(Access) -> watch_list_all_namespaces([], Access).
-watch_list_all_namespaces(Options, Access) ->
-    ?Core:http_stream_request(?ApiWatchPodsAllNs, Options, Access).
+watch_list_all_namespaces(Query, Access) ->
+    Resource = ?ApiWatchPodsAllNs,
+    ?Core:http_stream_request(Resource, Query, Access).
 
 watch(Ref) -> ?Core:http_stream_read(Ref).
 
 exec(Namespace, PodName, ContainerName, Command, Access) ->
+    exec(Namespace, PodName, ContainerName, Command, infinity, Access).
+
+exec(Namespace, PodName, ContainerName, Command, Timeout, Access) ->
+    Resource = {?ApiPodExec, [Namespace, PodName]},
+
     Query = [{"stdout", "true"},
              {"stderr", "true"},
              {"container", ContainerName}|
              [{"command", Arg} || Arg <- string:split(Command, " ", all)]],
 
-    ?Core:ws_request({?ApiPodExec, [Namespace, PodName]}, Query, Access).
+    Options = [{recv_timeout, Timeout}],
+
+    ?Core:ws_request(Resource, Query, Options, Access).
 
 read_log(Namespace, PodName, Access) ->
     read_log(Namespace, PodName, [], Access).
 
-read_log(Namespace, PodName, Options, Access) ->
-    ?Core:http_request({?ApiPodLog, [Namespace, PodName]}, Options, Access).
+read_log(Namespace, PodName, Query, Access) ->
+    Resource = {?ApiPodLog, [Namespace, PodName]},
+    ?Core:http_request(Resource, Query, Access).
