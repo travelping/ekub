05e8ebb8a27c71a4b7ba18f467ed6dcb3b3cac4c
Anton Alferov <anton@alferov.me>
2019-03-21 23:58:48 +0100

    Add type specs for api module

--- src/ekub_api.erl
+++ src/ekub_api.erl
@@ -14,28 +14,76 @@
     resource_types/1, resource_type/2,
     sub_resource/1,
 
-    load/1
+    load/1,
+    load_endpoints/1
+]).
+
+-export_types([
+    api/0,
+
+    group/0, group_name/0, group_version/0,
+    resource_alias/0,
+
+    namespace_name/0, namespace/0, name/0
 ]).
 
 -define(Core, ekub_core).
+-define(Access, ekub_access).
+
+-type endpoint() :: ?Core:endpoint().
+-type api_access() :: {api(), ?Access:access()}.
+
+-type api() :: #{
+    namespaced => sets:set(resource_type()),
+    aliases => #{resource_alias() => resource_type()},
+    groups => #{resource_type => group()}
+}.
+
+-type group() ::
+    {group_name(), group_version()} |
+    false.
+
+-type group_name() :: string().
+-type group_version() :: string().
+
+-type resource_alias() ::
+    {resource_ref(), sub_resource()} |
+    resource_ref().
+
+-type resource_ref() ::
+    resource_kind() |
+    resource_type() |
+    resource_plural_name() |
+    resource_singular_name().
+
+-type sub_resource() :: atom().
+
+-type resource_kind() :: atom().
+-type resource_type() :: atom().
+-type resource_plural_name() :: atom().
+-type resource_singular_name() :: atom().
+
+-type namespace_name() ::
+    {namespace(), name()} |
+    name() | % current namespace
+    {}. % all namespaces
+
+-type namespace() :: string().
+-type name() :: string().
 
 -define(ApiCoreEndpoint, <<"/api/v1">>).
 -define(ApisEndpoint, <<"/apis">>).
 
-%-type namespace_name() ::
-%    {namespace(), name()} |
-%    {namespace()} | % equal to {namespace(), ""}
-%    name() | % equal to {"", name()}
-%    {}.
-%
-%-type namespace() :: atom() | string() | binary().
-%-type name() :: atom() | string() | binary().
-
+-spec endpoint(resource_alias(), api_access()) -> endpoint().
 endpoint(ResourceAlias, {Api, Access}) ->
     endpoint(false, ResourceAlias, {"", ""}, {Api, Access}).
 
+-spec endpoint(resource_alias(), namespace_name(), api_access()) -> endpoint().
 endpoint(ResourceAlias, NamespaceName, {Api, Access}) ->
     endpoint(false, ResourceAlias, NamespaceName, {Api, Access}).
+
+-spec endpoint(group(), resource_alias(), namespace_name(), api_access()) ->
+    endpoint().
 
 endpoint(Group, ResourceAlias, NamespaceName, {Api, Access}) ->
     IsResource = is_resource(ResourceAlias, Api),
@@ -52,6 +100,8 @@ endpoint(Group, ResourceAlias, NamespaceName, {Api, Access}) ->
         );
     not IsResource -> "" end.
 
+-spec endpoint_build(group_name(), group_version(), resource_type(),
+                     namespace(), name(), sub_resource()) -> endpoint().
 endpoint_build(
     GroupName, GroupVersion, ResourceType,
     Namespace, Name, SubResource
@@ -69,6 +119,9 @@ endpoint_build(
            SubResource /= <<"">> -> <<$/, SubResource/binary>> end
     ]).
 
+-spec namespace(resource_alias(), namespace_name(), api_access()) ->
+    namespace().
+
 namespace(ResourceAlias, NamespaceName, {Api, Access}) ->
     Namespace = namespace(NamespaceName),
     IsNamespaced = is_namespaced(ResourceAlias, Api) andalso Namespace /= {},
@@ -79,45 +132,55 @@ namespace(ResourceAlias, NamespaceName, {Api, Access}) ->
         not IsNamespaceEmpty -> Namespace end;
     not IsNamespaced -> <<"">> end.
 
+-spec namespace(namespace_name()) -> namespace().
 namespace({}) -> {};
-namespace({Namespace}) -> Namespace;
 namespace({Namespace, _Name}) -> Namespace;
 namespace(_Name) -> <<"">>.
 
+-spec name(namespace_name()) -> name().
 name({}) -> <<"">>;
-name({_Namespace}) -> <<"">>;
 name({_Namespace, Name}) -> Name;
 name(Name) -> Name.
 
+-spec is_resource(resource_alias(), api()) -> boolean().
 is_resource(ResourceAlias, Api) ->
     maps:is_key(alias(ResourceAlias), maps:get(aliases, Api, #{})).
 
+-spec is_namespaced(resource_alias(), api()) -> boolean().
 is_namespaced(ResourceAlias, Api) ->
     ResourceType = resource_type(ResourceAlias, Api),
     sets:is_element(ResourceType, maps:get(namespaced, Api, sets:new())).
 
+-spec group(resource_alias(), api()) -> group().
 group(ResourceAlias, Api) ->
     ResourceType = resource_type(ResourceAlias, Api),
     maps:get(ResourceType, maps:get(groups, Api, #{})).
 
+-spec resource_types(api()) -> [resource_type()].
 resource_types(Api) ->
     [binary_to_list(Type) || Type <- maps:keys(maps:get(groups, Api, #{}))].
 
+-spec resource_type(resource_alias(), api()) -> resource_type().
 resource_type(ResourceAlias, Api) ->
     maps:get(alias(ResourceAlias), maps:get(aliases, Api, #{})).
 
+-spec sub_resource(resource_alias()) -> sub_resource().
 sub_resource({_ResourceRef, SubResource}) -> SubResource;
 sub_resource(_ResourceAlias) -> "".
 
 alias(Alias) when is_tuple(Alias) -> to_binary(Alias);
 alias(Alias) -> to_binary({Alias, <<"">>}).
 
+-spec load(?Access:access()) -> {ok, api()} |
+                                {error, Reason :: term()}.
 load(Access) ->
     case load_endpoints(Access) of
         {ok, Endpoints} -> load_api(Endpoints, Access);
         {error, Reason} -> {error, Reason}
     end.
 
+-spec load_endpoints(?Access:access()) -> {ok, [endpoint()]} |
+                                          {error, Reason :: term()}.
 load_endpoints(Access) ->
     case ?Core:http_request(?ApisEndpoint, Access) of
         {ok, Apis} -> {ok, [?ApiCoreEndpoint|[
